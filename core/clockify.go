2f4cbea7f9a65bf36e065514691f652adc318ccd
Tamara deMent <tamara.dement@gmail.com>
2026-08-20 13:08:51 -0400

    Configure golangci-lint and lint command

--- core/clockify.go
+++ core/clockify.go
@@ -100,7 +100,10 @@ func FetchClockifyRequests(c *ClockifyClient, workspaceID string, payload Clocki
 	if err != nil {
 		return nil, fmt.Errorf("http request: %w", err)
 	}
-	defer resp.Body.Close()
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	respBytes, err := io.ReadAll(resp.Body)
 	if err != nil {
